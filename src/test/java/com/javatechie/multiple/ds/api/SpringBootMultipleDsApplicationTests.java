760b2dcff89dda44cda638f061d1eedb208a71f3
sermsak.sukprasert@nttdata.com <sermsak.sukprasert@nttdata.com>
2023-09-05 13:40:01 +0700

    multi-datasource

---
+++ src/test/java/com/javatechie/multiple/ds/api/SpringBootMultipleDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.multiple.ds.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultipleDsApplicationTests {
+
+    @Test
+    void contextLoads() {
+	}
+
+}
